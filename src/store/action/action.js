197d9bd0f1e667e51b8cf36a7bafe0876b16eb21
Muhammad Sufyan <64953164+M-Sufyan22@users.noreply.github.com>
2021-02-06 01:56:18 +0500

    Delete src directory

--- src/store/action/action.js
+++
@@ -1,8 +0,0 @@
-const saveCurrentUser = (user) => {
-    console.log(user)
-    return (dispatch) => {
-        dispatch({ type: "saveCurrentUser", payload: user })
-    }
-};
-
-export { saveCurrentUser }
